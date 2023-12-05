e11304ebf03935be13827c3b4eab4f94859d8da7
ValterCarlens <valter.carlens@elev.ga.ntig.se>
2023-12-05 11:10:15 +0100

    Upg2.1 Färdig

--- Tasks/Uppgift2.1/script.js
+++ Tasks/Uppgift2.1/script.js
@@ -1,1 +1,11 @@
 console.log("Hello World");
+
+
+function button() {
+    alert("Hejsan");
+  }
+
+
+function ruta_function() {
+    document.getElementById("ruta").style.backgroundColor = "lightblue";
+}
